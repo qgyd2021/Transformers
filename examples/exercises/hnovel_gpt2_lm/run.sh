fd450c67dfa993092f7f933857ef7b156e762a85
晴耕雨读 <qgyd2021@gmail.com>
2023-08-22 18:01:32 +0800

    [update]hnovel

--- examples/exercises/hnovel_gpt2_lm/run.sh
+++ examples/exercises/hnovel_gpt2_lm/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# nohup sh run.sh --stage -1 --stop_stage 0 --system_version centos &
+# nohup sh run.sh --stage 0 --stop_stage 0 --system_version centos &
 # sh run.sh --stage 0 --stop_stage 1 --system_version windows
 # sh run.sh --stage 1 --stop_stage 1
 # sh run.sh --stage -1 --stop_stage 1
