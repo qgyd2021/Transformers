555a4e25bd7fddae78ca9326d2c66ca72015d905
晴耕雨读 <qgyd2021@gmail.com>
2023-08-23 09:56:27 +0800

    [update]hnovel

--- examples/exercises/hnovel_gpt2_lm/1.train_model.py
+++ examples/exercises/hnovel_gpt2_lm/1.train_model.py
@@ -33,19 +33,48 @@ def get_args():
     parser.add_argument("--dataset_path", default="qgyd2021/HNovel", type=str)
     parser.add_argument("--dataset_name", default="all", type=str)
     parser.add_argument("--dataset_split", default="train", type=str)
-
     parser.add_argument(
         "--dataset_cache_dir",
         default=(project_path / "hub_datasets").as_posix(),
         type=str
     )
 
-    parser.add_argument("--truncate_longer_samples", action="store_true")
-    parser.add_argument("--max_length", default=1024, type=int)
-    parser.add_argument("--batch_size", default=4, type=int)
-
     parser.add_argument("--cache_dir", default="file_dir/cache", type=str)
-    parser.add_argument("--output_dir", default="output_dir", type=str)
+
+    parser.add_argument("--output_dir", default="serialization_dir", type=str)
+    parser.add_argument("--overwrite_output_dir", action="store_true")
+    parser.add_argument("--evaluation_strategy", default="no", choices=["no", "steps", "epoch"], type=str)
+    parser.add_argument("--per_device_train_batch_size", default=4, type=int)
+    parser.add_argument("--gradient_accumulation_steps", default=4, type=int)
+    parser.add_argument("--learning_rate", default=1e-5, type=float)
+    parser.add_argument("--weight_decay", default=0, type=float)
+    parser.add_argument("--max_grad_norm", default=1.0, type=float)
+    parser.add_argument("--num_train_epochs", default=3.0, type=float)
+    parser.add_argument("--max_steps", default=-1, type=int)
+    parser.add_argument("--lr_scheduler_type", default="cosine", type=str)
+    parser.add_argument("--warmup_ratio", default=0.0, type=float)
+    parser.add_argument("--warmup_steps", default=3000, type=int)
+    parser.add_argument("--logging_steps", default=300, type=int)
+    parser.add_argument("--save_strategy", default="steps", type=str)
+    parser.add_argument("--save_steps", default=500, type=int)
+    parser.add_argument("--save_total_limit", default=3, type=int)
+    parser.add_argument("--no_cuda", action="store_true")
+    parser.add_argument("--seed", default=3407, type=str, help="https://arxiv.org/abs/2109.08203")
+    parser.add_argument("--fp16", action="store_true")
+    parser.add_argument("--half_precision_backend", default="auto", type=str)
+    parser.add_argument("--dataloader_num_workers", default=5, type=int)
+    parser.add_argument("--disable_tqdm", action="store_false")
+    parser.add_argument("--remove_unused_columns", action="store_false")
+    # parser.add_argument("--deepspeed", default="ds_z3_config.json", type=str)
+    parser.add_argument("--deepspeed", default=None, type=str)
+    parser.add_argument("--optim", default="adamw_hf", type=str)
+    parser.add_argument("--report_to", default="tensorboard", type=str)
+    parser.add_argument("--resume_from_checkpoint", default="file_dir/serialization_dir/checkpoint-103000", type=str)
+    parser.add_argument("--gradient_checkpointing", action="store_true")
+    # parser.add_argument("--gradient_checkpointing", action="store_false")
+
+    parser.add_argument("--truncate_longer_samples", action="store_true")
+    parser.add_argument("--max_seq_length", default=1024, type=int)
 
     args = parser.parse_args()
     return args
@@ -69,7 +98,7 @@ def main():
         outputs = tokenizer.__call__(examples['text'],
                                      truncation=True,
                                      padding='max_length',
-                                     max_length=args.max_length,
+                                     max_length=args.max_seq_length,
                                      return_special_tokens_mask=True)
         return outputs
 
@@ -81,12 +110,12 @@ def main():
     def group_texts(examples):
         concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
         total_length = len(concatenated_examples[list(examples.keys())[0]])
-        if total_length >= args.max_length:
-            total_length = (total_length // args.max_length) * args.max_length
+        if total_length >= args.max_seq_length:
+            total_length = (total_length // args.max_seq_length) * args.max_seq_length
         else:
-            raise AssertionError("total_length: {} < args.max_length: {}".format(args.max_length, args.max_length))
+            raise AssertionError("total_length: {} < args.max_seq_length: {}".format(args.max_seq_length, args.max_seq_length))
         result = {
-            k: [t[i: i + args.max_length] for i in range(0, total_length, args.max_length)]
+            k: [t[i: i + args.max_seq_length] for i in range(0, total_length, args.max_seq_length)]
             for k, t in concatenated_examples.items()
         }
         return result
@@ -122,17 +151,25 @@ def main():
 
     training_args = TrainingArguments(
         output_dir=args.output_dir,
-        evaluation_strategy="no",
-        overwrite_output_dir=True,
-        num_train_epochs=10,
-        per_device_train_batch_size=args.batch_size,
-        gradient_accumulation_steps=16,
-        logging_steps=1000,
-        save_steps=1000,
-        fp16=True if torch.cuda.is_available() else False,
-        local_rank=-1,
-        save_total_limit=5,
-        report_to="tensorboard"
+        overwrite_output_dir=args.overwrite_output_dir,
+        evaluation_strategy=args.evaluation_strategy,
+        per_device_train_batch_size=args.per_device_train_batch_size,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        learning_rate=args.learning_rate,
+        num_train_epochs=args.num_train_epochs,
+        max_steps=args.max_steps,
+        lr_scheduler_type=args.lr_scheduler_type,
+        warmup_steps=args.warmup_steps,
+        logging_steps=args.logging_steps,
+        save_steps=args.save_steps,
+        save_total_limit=args.save_total_limit,
+        no_cuda=args.no_cuda,
+        fp16=args.fp16,
+        half_precision_backend=args.half_precision_backend,
+        # deepspeed=args.deepspeed,
+        report_to=args.report_to,
+        resume_from_checkpoint=args.resume_from_checkpoint,
+        gradient_checkpointing=args.gradient_checkpointing,
     )
 
     trainer = Trainer(
