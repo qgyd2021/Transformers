20b25f57f5360104e84290e4bd0babcae5a86052
晴耕雨读 <qgyd2021@gmail.com>
2023-08-25 16:25:12 +0800

    [update]toolbox add firefly

--- examples/exercises/firefly_chatglm2_6b_intent/README.md
+++ examples/exercises/firefly_chatglm2_6b_intent/README.md
@@ -1,3 +1,9 @@
 ## 电话营销场景意图识别
 
 基于 [firefly-chatglm2-6b](https://huggingface.co/YeungNLP/firefly-chatglm2-6b) 模型做训练. 
+
+
+```text
+https://github.com/yangjianxin1/Firefly/blob/master/train_args/qlora/chatglm2-6b-sft-qlora.json
+
+```
--- examples/exercises/firefly_chatglm2_6b_intent/run.sh
+++ examples/exercises/firefly_chatglm2_6b_intent/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# sh run.sh --stage -1 --stop_stage 2 --system_version centos --pretrained_model_name bloom-1b4-zh --final_model_name bloom-1b4-sft
-# sh run.sh --stage -1 --stop_stage 1 --system_version centos --pretrained_model_name bloom-1b4-zh
-# sh run.sh --stage 1 --stop_stage 1 --system_version centos --pretrained_model_name bloom-1b4-zh
-# sh run.sh --stage 2 --stop_stage 2 --system_version centos --pretrained_model_name bloom-1b4-zh --final_model_name bloom-1b4-sft
+# sh run.sh --stage -1 --stop_stage -1 --system_version centos
+# sh run.sh --stage -1 --stop_stage 1 --system_version centos
+# sh run.sh --stage 1 --stop_stage 1 --system_version centos
+# sh run.sh --stage 2 --stop_stage 2 --system_version centos
 
 # sh run.sh --stage 1 --stop_stage 1 --system_version windows --pretrained_model_name bloom-1b4-zh
 
