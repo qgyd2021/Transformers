2857755e0c343897a0053f266f4d27420360542a
晴耕雨读 <qgyd2021@gmail.com>
2023-09-01 16:00:08 +0800

    [update]20230901155700

--- examples/exercises/gpt2_chinese_h_novel/2.train_model.py
+++ examples/exercises/gpt2_chinese_h_novel/2.train_model.py
@@ -83,12 +83,12 @@ def main():
     train_data_files = [args.train_subset]
     dataset_dict["train"] = load_dataset(
         path="json", data_files=[str(file) for file in train_data_files],
-        streaming=True,
+        # streaming=True,
     )["train"]
     valid_data_files = [args.valid_subset]
     dataset_dict["valid"] = load_dataset(
         path="json", data_files=[str(file) for file in valid_data_files],
-        streaming=True,
+        # streaming=True,
     )["train"]
 
     print(dataset_dict)
