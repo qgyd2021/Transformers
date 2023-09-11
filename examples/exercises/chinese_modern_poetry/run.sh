cd439ac92c6d1126b483709330b53536f720ea68
晴耕雨读 <qgyd2021@gmail.com>
2023-09-11 12:39:00 +0800

    [update]add test model

--- examples/exercises/chinese_modern_poetry/run.sh
+++ examples/exercises/chinese_modern_poetry/run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # sh run.sh --stage 0 --stop_stage 0 --system_version centos
+# sh run.sh --stage 1 --stop_stage 2 --system_version centos
 
+# bitsandbytes
+export LD_LIBRARY_PATH="/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 
 # params
 system_version="windows";
