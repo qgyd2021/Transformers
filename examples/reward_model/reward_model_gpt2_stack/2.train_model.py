0d6f661404b6ddb3f78165a473519c5ad227db9e
晴耕雨读 <qgyd2021@gmail.com>
2023-09-21 16:21:38 +0800

    [update]add merge lora

--- examples/reward_model/reward_model_gpt2_stack/2.train_model.py
+++ examples/reward_model/reward_model_gpt2_stack/2.train_model.py
@@ -33,73 +33,61 @@ from transformers import (
     TrainingArguments,
 )
 from transformers.utils import PaddingStrategy
+from transformers.models.gpt2.modeling_gpt2 import GPT2ForSequenceClassification
+from transformers.trainer_utils import EvalPrediction, IntervalStrategy
 
 from project_settings import project_path
 
 
 @dataclass
 class ScriptArguments:
-    local_rank: Optional[int] = field(default=-1, metadata={"help": "Used for multi-gpu"})
-    resume_from_checkpoint: Optional[bool] = field(
-        default=False,
-        metadata={"help": "If you want to resume training where it left off."},
-    )
-    deepspeed: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": "Path to deepspeed config if using deepspeed. "
-                    "You may need this if the model that you want to train doesn't fit on a single GPU."
-        },
-    )
+    # dataset
+    dataset_path: str = field(default="lvwerra/stack-exchange-paired")
+    dataset_cache_dir: str = field(default=(project_path / "hub_datasets").as_posix())
+    train_subset: Optional[int] = field(default=-1)
+    eval_subset: Optional[int] = field(default=10000)
+
+    # model
+    model_name: Optional[str] = field(default="gpt2")
+    # tokenizer
+    tokenizer_name: Optional[str] = field(default=None)
+
+    # dataset process
+    max_length: Optional[int] = field(default=512)
+
+    # lora
+    lora_rank: int = field(default=64)
+    lora_alpha: int = field(default=32)
+    lora_dropout: float = field(default=0.05)
+
+    # training_args
+    output_dir: Optional[str] = field(default="output_dir")
+    evaluation_strategy: Union[IntervalStrategy, str] = field(default="steps")
     per_device_train_batch_size: Optional[int] = field(default=4)
     per_device_eval_batch_size: Optional[int] = field(default=1)
     gradient_accumulation_steps: Optional[int] = field(default=1)
     learning_rate: Optional[float] = field(default=2e-5)
     weight_decay: Optional[float] = field(default=0.001)
-    model_name: Optional[str] = field(
-        default="gpt2",
-        metadata={
-            "help": "The model that you want to train from the Hugging Face hub. E.g. gpt2, gpt2-xl, bert, etc."
-        },
-    )
-    tokenizer_name: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": "The tokenizer for your model, if left empty will use the default for your model",
-        },
-    )
-    bf16: Optional[bool] = field(
-        default=False,
-        metadata={
-            "help": "This essentially cuts the training time in half "
-                    "if you want to sacrifice a little precision and have a supported GPU."
-        },
-    )
-    num_train_epochs: Optional[int] = field(
-        default=1,
-        metadata={"help": "The number of training epochs for the reward model."},
-    )
-    train_subset: Optional[int] = field(
-        default=100000,
-        metadata={"help": "The size of the subset of the training data to use"},
-    )
-    eval_subset: Optional[int] = field(
-        default=50000,
-        metadata={"help": "The size of the subset of the eval data to use"},
-    )
-    gradient_checkpointing: Optional[bool] = field(
-        default=False,
-        metadata={"help": "Enables gradient checkpointing."},
-    )
-    optim: Optional[str] = field(
-        default="adamw_hf",
-        metadata={"help": "The optimizer to use."},
-    )
-    lr_scheduler_type: Optional[str] = field(
-        default="linear",
-        metadata={"help": "The lr scheduler"},
-    )
-    max_length: Optional[int] = field(default=512)
+    num_train_epochs: float = field(default=1.0)
+    lr_scheduler_type: Optional[str] = field(default="linear")
+    logging_strategy: Union[IntervalStrategy, str] = field(default="steps")
+    save_strategy: Union[IntervalStrategy, str] = field(default="steps")
+    logging_steps: float = field(default=500)
+    bf16: bool = field(default=False)
+    fp16: bool = field(default=False)
+    local_rank: Optional[int] = field(default=-1, metadata={"help": "Used for multi-gpu"})
+    eval_steps: Optional[float] = field(default=5000)
+    save_steps: float = field(default=500)
+    save_total_limit: Optional[int] = field(default=5)
+    remove_unused_columns: Optional[bool] = field(default=False)
+    label_names: Optional[List[str]] = field(default=None)
+    deepspeed: Optional[str] = field(default=None)
+    optim: Optional[str] = field(default="adamw_hf")
+    report_to: Optional[List[str]] = field(default=None)
+    resume_from_checkpoint: Optional[bool] = field(default=False)
+    gradient_checkpointing: Optional[bool] = field(default=False)
+
+    # addition
     eval_first_step: Optional[bool] = field(
         default=False,
         metadata={"help": "Whether to run eval after the first step"},
@@ -108,18 +96,10 @@ class ScriptArguments:
 
 def get_args():
     parser = HfArgumentParser(ScriptArguments)
-
-    parser.add_argument("--dataset_path", default="lvwerra/stack-exchange-paired", type=str)
-    parser.add_argument(
-        "--dataset_cache_dir",
-        default=(project_path / "hub_datasets").as_posix(),
-        type=str
-    )
-    args = parser.parse_args()
+    args = parser.parse_args_into_dataclasses()[0]
     return args
 
 
-# We need to define a special data collator that batches the data in our j vs k format.
 @dataclass
 class RewardDataCollatorWithPadding:
     tokenizer: PreTrainedTokenizerBase
@@ -132,18 +112,14 @@ class RewardDataCollatorWithPadding:
         features_j = []
         features_k = []
         for feature in features:
-            features_j.append(
-                {
-                    "input_ids": feature["input_ids_j"],
-                    "attention_mask": feature["attention_mask_j"],
-                }
-            )
-            features_k.append(
-                {
-                    "input_ids": feature["input_ids_k"],
-                    "attention_mask": feature["attention_mask_k"],
-                }
-            )
+            features_j.append({
+                "input_ids": feature["input_ids_j"],
+                "attention_mask": feature["attention_mask_j"],
+            })
+            features_k.append({
+                "input_ids": feature["input_ids_k"],
+                "attention_mask": feature["attention_mask_k"],
+            })
         batch_j = self.tokenizer.pad(
             features_j,
             padding=self.padding,
@@ -169,11 +145,12 @@ class RewardDataCollatorWithPadding:
 
 
 class RewardTrainer(Trainer):
-    # Define how to compute the reward loss. We use the InstructGPT pairwise logloss: https://arxiv.org/abs/2203.02155
+    # Define how to compute the reward loss.
+    # We use the InstructGPT pairwise logloss: https://arxiv.org/abs/2203.02155
     def compute_loss(self, model, inputs, return_outputs=False):
         rewards_j = model(input_ids=inputs["input_ids_j"], attention_mask=inputs["attention_mask_j"])[0]
         rewards_k = model(input_ids=inputs["input_ids_k"], attention_mask=inputs["attention_mask_k"])[0]
-        loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
+        loss = - nn.functional.logsigmoid(rewards_j - rewards_k).mean()
         if return_outputs:
             return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
         return loss
@@ -189,7 +166,6 @@ def main():
     args = get_args()
 
     # dataset
-    # Load the human stack-exchange-paired dataset for tuning the reward model.
     train_dataset = load_dataset(
         path=args.dataset_path,
         data_dir="data/reward",
@@ -207,55 +183,54 @@ def main():
     if args.eval_subset > 0:
         eval_dataset = eval_dataset.select(range(args.eval_subset))
 
-    # Define the training args. Needs to be done before the model is loaded if you are using deepspeed.
-    model_name_split = args.model_name.split("/")[-1]
-    output_name = (
-        f"{model_name_split}_peft_stack-exchange-paired_rmts__{args.train_subset}_{args.learning_rate}"
-    )
-
+    # training_args
     training_args = TrainingArguments(
-        output_dir=output_name,
-        learning_rate=args.learning_rate,
+        output_dir=args.output_dir,
+        evaluation_strategy=args.evaluation_strategy,
         per_device_train_batch_size=args.per_device_train_batch_size,
         per_device_eval_batch_size=args.per_device_eval_batch_size,
-        num_train_epochs=args.num_train_epochs,
-        weight_decay=args.weight_decay,
-        evaluation_strategy="steps",
-        eval_steps=500,
-        save_strategy="steps",
-        save_steps=500,
         gradient_accumulation_steps=args.gradient_accumulation_steps,
-        gradient_checkpointing=args.gradient_checkpointing,
-        deepspeed=args.deepspeed,
-        local_rank=args.local_rank,
-        remove_unused_columns=False,
-        label_names=[],
-        bf16=args.bf16,
-        logging_strategy="steps",
-        logging_steps=10,
-        optim=args.optim,
+        learning_rate=args.learning_rate,
+        weight_decay=args.weight_decay,
+        num_train_epochs=args.num_train_epochs,
         lr_scheduler_type=args.lr_scheduler_type,
+        logging_strategy=args.logging_strategy,
+        logging_steps=args.logging_steps,
+        save_strategy=args.save_strategy,
+        bf16=args.bf16,
+        fp16=args.fp16,
+        local_rank=args.local_rank,
+        eval_steps=args.eval_steps,
+        save_steps=args.save_steps,
+        save_total_limit=args.save_total_limit,
+        remove_unused_columns=args.remove_unused_columns,
+        label_names=args.label_names,
+        deepspeed=args.deepspeed,
+        optim=args.optim,
+        report_to=args.report_to,
+        resume_from_checkpoint=args.resume_from_checkpoint,
+        gradient_checkpointing=args.gradient_checkpointing,
     )
 
-    # Load the value-head model and tokenizer.
+    # tokenizer
     tokenizer_name = args.tokenizer_name if args.tokenizer_name is not None else args.model_name
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_auth_token=True)
 
-    tokenizer.pad_token = tokenizer.eos_token
-
-    peft_config = LoraConfig(
-        task_type=TaskType.SEQ_CLS,
-        inference_mode=False,
-        r=8,
-        lora_alpha=32,
-        lora_dropout=0.1,
-    )
-
+    # model
     model = AutoModelForSequenceClassification.from_pretrained(
         args.model_name,
         num_labels=1,
         torch_dtype=torch.bfloat16
     )
+
+    peft_config = LoraConfig(
+        task_type=TaskType.SEQ_CLS,
+        inference_mode=False,
+        r=args.lora_rank,
+        lora_alpha=args.lora_alpha,
+        lora_dropout=args.lora_dropout,
+    )
+
     model = get_peft_model(model, peft_config)
     model.print_trainable_parameters()
 
@@ -263,7 +238,6 @@ def main():
     tokenizer.pad_token = tokenizer.eos_token
     model.config.pad_token_id = tokenizer.eos_token_id
     model.config.use_cache = not args.gradient_checkpointing
-    num_proc = os.cpu_count() // 2
     original_columns = train_dataset.column_names
 
     # Turn the dataset into pairs of post + summaries,
@@ -291,7 +265,7 @@ def main():
     train_dataset = train_dataset.map(
         preprocess_function,
         batched=True,
-        num_proc=num_proc,
+        num_proc=os.cpu_count() // 2,
         remove_columns=original_columns,
     )
     train_dataset = train_dataset.filter(
@@ -301,7 +275,7 @@ def main():
     eval_dataset = eval_dataset.map(
         preprocess_function,
         batched=True,
-        num_proc=num_proc,
+        num_proc=os.cpu_count() // 2,
         remove_columns=original_columns,
     )
     eval_dataset = eval_dataset.filter(
@@ -311,7 +285,7 @@ def main():
     # Define the metric that we'll use for validation.
     accuracy = evaluate.load("accuracy")
 
-    def compute_metrics(eval_pred):
+    def compute_metrics(eval_pred: EvalPrediction) -> Dict[str, Any]:
         predictions, _ = eval_pred
         # Here, predictions is rewards_j and rewards_k.
         # We want to see how much of the time rewards_j > rewards_k.
@@ -335,7 +309,7 @@ def main():
     trainer.train(args.resume_from_checkpoint)
 
     print("Saving last checkpoint of the model")
-    model.save_pretrained(output_name + "_peft_last_checkpoint")
+    model.save_pretrained("last_checkpoint")
     return
 
 
